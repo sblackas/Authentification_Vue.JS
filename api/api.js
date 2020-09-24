5f321cafffaaf7aac705f144982c0fb0fbb9f7e1
sblackas <vivianne.traore@gmail.com>
2020-09-24 17:19:58 +0200

    save

--- api/api.js
+++ api/api.js
@@ -3,12 +3,21 @@ const api = express();
 const bodyParser = require('body-parser')
 // const mysql = require('mysql2');
 const routes = require("./src/routes/routes")
+const cors = require('cors');
 
+// CORS middleware
+api.use(cors());
+const allowCrossDomain = function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', '*');
+  res.header('Access-Control-Allow-Headers', '*');
+  next();
+}
+api.use(allowCrossDomain)
 
 api.use(bodyParser.json());
 api.use(bodyParser.urlencoded({extended: true}));
 api.use("/", routes);
-
 
 
 api.listen(8000, function () {
--- api/src/routes/routes.js
+++ api/src/routes/routes.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const db = require('../database/db');
 const bcrypt = require('bcrypt');
 const saltRounds = 10; // le nombre de fois que l'on hashe le mdp
+const config = require('./config');
+const jwt = require('jsonwebtoken');
 
 
 
@@ -16,7 +18,15 @@ router.post('/', function(req, res) {
         db.query(newUser, function (err, result) { // envoyer mon newUser dans ma database
             if (err) throw err;
             console.log("one user inserted");
-            res.send(result);
+
+
+            db.query(`SELECT * FROM users WHERE email = '${req.body.email}'`, function (err, result) { 
+              if (err) throw err;
+              let token = jwt.sign({ id: result[0].id }, config.secret, {expiresIn: 86400 });
+              console.log(token);
+              res.status(200).send({ auth: true, token: token, user: result[0] });
+            })
+            
     
             });
     
@@ -33,20 +43,24 @@ router.post('/', function(req, res) {
       db.query(`SELECT * FROM users WHERE email = '${req.body.email}'`, function (err, result) { // *=tout
         if (err) throw err;
         bcrypt.compare(req.body.password, result[0].password, function(err,resultat){
-console.log(result);
-if(resultat) {
-  res.send("you are authenticated")
-} else {
-  res.send("sorry we don't know this user")
-}
+            console.log(resultat);
+            if(resultat) {
+              let token = jwt.sign({ id: result[0].id }, config.secret, { expiresIn: 86400 });
+              console.log(token);
+              res.send({ auth: true, token: token, user: result[0] }); 
+            } else {
+              res.status(400).send("sorry we don't know this user") //rajout de .status(400)
+            }
 
         })
-        // if (req.body.password === result[0].password)
-        // console.log("you are authenticated"); 
-        // else 
-        // console.log("Sorry, we don't know this user");
-        // res.send(result);
-        
+
+
+        // let passwordIsValid = bcrypt.compareSync(req.body.password, result[0].password);
+        // if (!passwordIsValid) return res.status(401).send({ auth: false, token: null });
+        // let token = jwt.sign({ id: result[0].id }, config.secret, { expiresIn: 86400 // expires in 24 hours
+        // });
+        // console.log(token);
+        // res.status(200).send({ auth: true, token: token, user: result[0] });
       });
 
     
